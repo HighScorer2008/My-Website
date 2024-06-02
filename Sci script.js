8a0b82c6aac24475754efc21cf76dbcd2559bce4
HighScorer2008 <r.m.dharungopesh@gmail.com>
2024-06-02 20:27:44 +0530

    Update Sci script.js

--- Sci script.js
+++ Sci script.js
@@ -63,4 +63,9 @@ function updateTheme() {
     footer.classList.toggle("dark-mode");
 }
 
+document.getElementById('modeToggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    this.textContent = document.body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
+});
+
 
